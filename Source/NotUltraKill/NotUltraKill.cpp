a53dcf8c23322d1603ab30856fc5505d86715354
pankeczap <50746281+pankeczap@users.noreply.github.com>
2024-04-28 23:57:21 +0100

    Initial commit

---
+++ Source/NotUltraKill/NotUltraKill.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "NotUltraKill.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NotUltraKill, "NotUltraKill" );
